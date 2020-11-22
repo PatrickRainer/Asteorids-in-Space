2b2a7637a563f31405f8c8162c97c932c7f03db2
Patrick <patrick.rainer2@gmail.com>
2020-11-22 07:46:04 +0100

    Updated Movment
    Movement by input manager
    
    
    Added a mouse listener
    
    
    Added a Mouse input listener
    
    
    Rotate towards mouse, untested
    
    
    Rotation to mouse works, but very directly
    
    
    Fixed Bug, Mousechanged event
    
    
    Implemented trhottle bar mechanic
    
    
    Adjusted rigidbody
    
    
    Deleted Left and Right Movement

---
+++ Assets/Scripts/CoreMechanics/InputSystem/InputListener.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CoreMechanics.InputSystem
+{ //TODO: Should depend on input scheme
+    public class InputListener : MonoBehaviour
+    {
+        MouseListener _mouseListener;
+        public UnityAction DownButtonPressed = delegate { };
+        public UnityAction LeftButtonPressed = delegate { };
+
+        public UnityAction<Vector3> MousePositionChanged = delegate(Vector3 arg0) {  };
+
+        public UnityAction RightButtonPressed = delegate { };
+
+        public UnityAction ShootButtonPressed = delegate
+        {
+            /*Debug.Log("Shoot Key");*/
+        };
+
+        public UnityAction UpButtonPressed = delegate { };
+
+        void Awake()
+        {
+            _mouseListener = gameObject.AddComponent<MouseListener>();
+            _mouseListener.MousePositionChanged += delegate(Vector3 newMousePos) {  MousePositionChanged.Invoke(newMousePos);};
+        }
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space)) ShootButtonPressed.Invoke();
+
+            if (Input.GetKey(KeyCode.A)) LeftButtonPressed.Invoke();
+            if (Input.GetKey(KeyCode.D)) RightButtonPressed.Invoke();
+            if (Input.GetKey(KeyCode.W)) UpButtonPressed.Invoke();
+            if (Input.GetKey(KeyCode.S)) DownButtonPressed.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/CoreMechanics/InputSystem/MouseListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CoreMechanics.InputSystem
+{
+    public class MouseListener : MonoBehaviour
+    {
+        Vector3 _currentMousePosition;
+        Camera _mainCamera;
+        Vector3 _worldMousePosition;
+
+        public UnityAction<Vector3> MousePositionChanged = delegate { };
+
+        void Awake()
+        {
+            _currentMousePosition = Input.mousePosition;
+        }
+
+        void Start()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        void Update()
+        {
+            _worldMousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - new Vector3(0, 0, -10);
+
+            if (Input.mousePosition != _currentMousePosition) MousePositionChanged.Invoke(_worldMousePosition);
+            //Debug.Log(MousePosition.ToString());
+        }
+    }
+}
