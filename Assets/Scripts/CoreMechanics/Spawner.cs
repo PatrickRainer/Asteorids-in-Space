c54522aace5ff743cae7a5154a94c7d943b2743a
Patrick <patrick.rainer2@gmail.com>
2020-11-22 20:36:33 +0100

    Spawning Asteorids around a cirle outside of the screen
    Asteorids moving towards center without rotation
    
    
    Asteorid moves to center and ignores rotation
    
    
    Asteorids moves further his way after reaching the center
    
    
    Asteorids back to Spawn Collection

--- Assets/Scripts/CoreMechanics/Spawner.cs
+++ Assets/Scripts/CoreMechanics/Spawner.cs
@@ -12,58 +12,55 @@ namespace CoreMechanics
         [SerializeField] [Title("Objects to Spawn")] [AssetsOnly]
         List<GameObject> spawnObjects = new List<GameObject>();
 
-        [ShowInInspector, ReadOnly] float _currentSpawnInterval;
+        [SerializeField] float spawnExtraRadius = 0.5f;
+        readonly Random _random = new Random();
+
+        [ShowInInspector] [ReadOnly] float _currentSpawnInterval;
+        DifficultyController _difficultyController;
 
         GameManager _gameManager;
-        readonly Random _random = new Random();
-        DifficultyController _difficultyController;
 
         void Start()
         {
             //Positioning at Top of Screen
             gameObject.transform.position = Positioner.GetScreenTopMiddle();
-            
+
             _gameManager = FindObjectOfType<GameManager>();
             _currentSpawnInterval = FindObjectOfType<GameManager>().startSpawnInterval;
             _difficultyController = FindObjectOfType<DifficultyController>();
             StartCoroutine(nameof(SpawnRandom));
-            
+
             _difficultyController.IncreaseDifficulty += IncreaseDifficulty;
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(Vector3.zero, GetSpawnRadius());
         }
 
         void IncreaseDifficulty(float stepToIncrease, float minSpawnInterval)
         {
-           //Debug.Log("Increase Difficulty");
-           if (_currentSpawnInterval <= minSpawnInterval) return;
+            //Debug.Log("Increase Difficulty");
+            if (_currentSpawnInterval <= minSpawnInterval) return;
 
-           _currentSpawnInterval -= stepToIncrease;
+            _currentSpawnInterval -= stepToIncrease;
+        }
+
+        float GetSpawnRadius()
+        {
+            var spawnRadius = Positioner.GetDistanceToScreenEdge() + spawnExtraRadius;
+            return spawnRadius;
         }
 
         IEnumerator SpawnRandom()
         {
             while (_currentSpawnInterval > 0)
             {
-                //Object
+                //Random Object
                 var rndObject = spawnObjects[_random.Next(0, spawnObjects.Count)];
 
-                //Main Camera
-                var mainCamera = Camera.main;
-                if (mainCamera is null) yield break;
-
-                //Screen Positions
-                var leftTopCorner =
-                    mainCamera.ScreenToWorldPoint(new Vector3(0, Screen.height,
-                        mainCamera.nearClipPlane));
-                var rightTopCorner =
-                    mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.nearClipPlane));
-                var topOfScreen = leftTopCorner.y;
-
-                //Start Position
-                var randomPosition = _random.Next((int) leftTopCorner.x, (int) rightTopCorner.x);
-                var startPos = new Vector2(randomPosition, topOfScreen);
-
-                //Instantiate
-                var iObj = Instantiate(rndObject, startPos, Quaternion.identity);
+                // Spawn around circle, outside of the screen
+                Instantiate(rndObject, MathExtras.RandomCircle(Vector3.zero, GetSpawnRadius()), Quaternion.identity);
 
                 yield return new WaitForSeconds(_currentSpawnInterval);
             }
