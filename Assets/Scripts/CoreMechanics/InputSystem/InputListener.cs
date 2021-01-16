52e69eb1c1dc9c77328dcdfa04ad595a0016a754
Patrick <patrick.rainer2@gmail.com>
2021-01-16 10:46:14 +0100

    Code Cleanup in InputListener

--- Assets/Scripts/CoreMechanics/InputSystem/InputListener.cs
+++ Assets/Scripts/CoreMechanics/InputSystem/InputListener.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 namespace CoreMechanics.InputSystem
-{ //TODO: Should depend on input scheme
+{ //TODO: Should depend on input scheme (PC, Android, IOS, etc)
     public class InputListener : MonoBehaviour
     {
         MouseListener _mouseListener;
-        public UnityAction BrakeButtonPressed = delegate { };
-        public UnityAction LeftButtonPressed = delegate { };
-
-        public UnityAction<Vector3> MousePositionChanged = delegate(Vector3 arg0) {  };
-
-        public UnityAction RightButtonPressed = delegate { };
-
-        public UnityAction ShootButtonPressed = delegate
-        {
-            /*Debug.Log("Shoot Key");*/
-        };
         
+        // Movement - Rotating //
+
+        public UnityAction<Vector3> MousePositionChanged = delegate {  };
+        public UnityAction RotateButtonPressed = delegate { };
+        public UnityAction RotateButtonReleased = delegate { };
+        public UnityAction RotateButtonDown= delegate { };
+        
+        // Movement - Sliding //
+        public UnityAction LeftButtonPressed = delegate { };
+        public UnityAction RightButtonPressed = delegate { };
+        
+        // Movement - Throttling //
+        public UnityAction BrakeButtonPressed = delegate { };
+        public UnityAction ThrottleButtonPressed = delegate { };
+        public UnityAction ThrottleButtonReleased = delegate { };
+        
+        // Shooting //
+        public UnityAction ShootButtonPressed = delegate { };
         public UnityAction MissileButtonPressed = delegate { };
         public UnityAction RocketButtonPressed = delegate { };
         public UnityAction ClusterBombButtonPressed = delegate { };
         
-
-        public UnityAction ThrottleButtonPressed = delegate { };
-        public UnityAction ThrottleButtonReleased = delegate { };
-        public UnityAction RotateButtonPressed = delegate { };
-        public UnityAction RotateButtonReleased = delegate { };
-        public UnityAction RotateButtonDown= delegate { };
-
         void Awake()
         {
             _mouseListener = gameObject.AddComponent<MouseListener>();
