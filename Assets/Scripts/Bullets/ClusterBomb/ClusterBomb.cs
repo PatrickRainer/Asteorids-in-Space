aee278e326c7369f5e1b803552d4347dd2c398c8
Patrick <patrick.rainer2@gmail.com>
2021-01-16 06:25:09 +0100

    Adjustment on the cluster bomb, it's technically working

--- Assets/Scripts/Bullets/ClusterBomb/ClusterBomb.cs
+++ Assets/Scripts/Bullets/ClusterBomb/ClusterBomb.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using DG.Tweening;
-using Extensions;
 using Sirenix.OdinInspector;
-using Unity.Mathematics;
 using UnityEngine;
 
 namespace Bullets
@@ -11,40 +9,40 @@ namespace Bullets
     {
         [SerializeField] [Required] [AssetsOnly]
         GameObject miniCluster;
-        
-        [SerializeField] [Required]  [SceneObjectsOnly]
-        List<Transform> miniClusterPositions = new List<Transform>(); 
 
-        Vector2 _startPosition;
+        [SerializeField] [Required] List<Transform> miniClusterPositions = new List<Transform>();
+        [SerializeField] int flyingDistance = 2;
+
+        [SerializeField] int clusterBombFlyingDuration = 1;
+
         Vector2 _endPosition; //Should move until this position and then stop
-        float percentOfWay = 0;
+
 
         protected override void Awake()
         {
             // Set Start and Target position
             var position = transform.position;
-            _startPosition = position;
-            _endPosition = position + new Vector3(0, 2);
+            _endPosition = position + new Vector3(0, flyingDistance);
 
             base.Awake();
         }
 
         protected override void OnDestroy()
         {
-            //Create mini clusters
-            foreach (var position in miniClusterPositions)
-            {
-                var go = Instantiate(miniCluster, position.position, Quaternion.identity);
-            }
+            gameObject.SetActive(false);
 
-            _rb.AddExplosionForce(3, transform.position, 3);
+            //Create mini clusters
+            foreach (var trf in miniClusterPositions)
+            {
+                var unused = Instantiate(miniCluster, trf.position, Quaternion.identity);
+            }
 
             base.OnDestroy();
         }
 
         protected override void Move()
         {
-            _rb.DOMove(_endPosition, 2);
+            _rb.DOMove(_endPosition, clusterBombFlyingDuration);
         }
     }
 }
